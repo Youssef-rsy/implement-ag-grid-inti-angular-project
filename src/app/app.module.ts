014714fc816c17da44b6ad8d3455f65c3a30d0b9
RossamyYoussef <youssef.rsy@gmail.com>
2018-04-20 17:08:58 +0100

    adding some filter to the ag-gride

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+/*************adding ag grid **********/
+import {Grid} from 'ag-grid';
+import {FormsModule} from '@angular/forms';
+import 'ag-grid-enterprise';
+import {AgGridModule} from 'ag-grid-angular/main';
 import { AppComponent } from './app.component';
+import { MaydatatableComponent } from './maydatatable/maydatatable.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaydatatableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
